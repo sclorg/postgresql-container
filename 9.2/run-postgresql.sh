969762b3c99eb6beee5147f76c29c8f480fb787b
Martin Nagy <nagy.martin@gmail.com>
2015-03-24 14:14:29 +0100

    Adding basic file configuration options
    Separate initdb-generated config and our custom configuration that
    is recreated on each container restart, according to the provided
    environment variables, similarly to the MySQL image.
    
    Added two settings, both from OpenShift v2 PostgreSQL cartridge:
    
     * POSTGRESQL_MAX_CONNECTIONS
     * POSTGRESQL_SHARED_BUFFERS

--- 9.2/run-postgresql.sh
+++ 9.2/run-postgresql.sh
@@ -6,7 +6,12 @@ source $HOME/.bashrc
 set -eu
 
 # Data dir
-export PGDATA=/var/lib/pgsql/data
+export PGDATA=$HOME/data
+POSTGRESQL_CONFIG_FILE=$HOME/openshift-custom-postgresql.conf
+
+# Configuration settings.
+export POSTGRESQL_MAX_CONNECTIONS=${POSTGRESQL_MAX_CONNECTIONS:-100}
+export POSTGRESQL_SHARED_BUFFERS=${POSTGRESQL_SHARED_BUFFERS:-32MB}
 
 # Be paranoid and stricter than we should be.
 psql_identifier_regex='^[a-zA-Z_][a-zA-Z0-9_]*$'
@@ -22,6 +27,9 @@ function usage() {
 	echo "  \$POSTGRESQL_DATABASE (regex: '$psql_identifier_regex')"
 	echo "Optional:"
 	echo "  \$POSTGRESQL_ADMIN_PASSWORD (regex: '$psql_password_regex')"
+	echo "Settings:"
+	echo "  \$POSTGRESQL_MAX_CONNECTIONS (default: 100)"
+	echo "  \$POSTGRESQL_SHARED_BUFFERS (default: 32MB)"
 	exit 1
 }
 
@@ -59,12 +67,8 @@ function initialize_database() {
 	# PostgreSQL configuration.
 	cat >> "$PGDATA/postgresql.conf" <<-EOF
 
-		#
-		# Custom OpenShift configuration starting at this point.
-		#
-
-		# Listen on all interfaces.
-		listen_addresses = '*'
+		# Custom OpenShift configuration:
+		include '../openshift-custom-postgresql.conf'
 	EOF
 
 	# Access control configuration.
@@ -90,10 +94,12 @@ function initialize_database() {
 	pg_ctl stop
 }
 
-if [ "$1" = "postgres" ]; then
-	if [ ! -f "$PGDATA/postgresql.conf" ]; then
-		initialize_database
-	fi
+# New config is generated every time a container is created. It only contains
+# additional custom settings and is included from $PGDATA/postgresql.conf.
+envsubst < ${POSTGRESQL_CONFIG_FILE}.template > ${POSTGRESQL_CONFIG_FILE}
+
+if [ "$1" = "postgres" -a ! -f "$PGDATA/postgresql.conf" ]; then
+	initialize_database
 fi
 
 unset_env_vars
