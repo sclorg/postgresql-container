bc30bbc6b67f2101bcb336034c5fdbba53516bd1
Ben Parees <bparees@redhat.com>
2015-11-06 12:28:18 -0500

    use variable in nss_wrapper setting

--- 9.4/root/usr/share/container-scripts/postgresql/common.sh
+++ 9.4/root/usr/share/container-scripts/postgresql/common.sh
@@ -74,7 +74,7 @@ function generate_passwd_file() {
   export GROUP_ID=$(id -g)
   envsubst < "${CONTAINER_SCRIPTS_PATH}/passwd.template" > "${HOME}/passwd"
   export LD_PRELOAD=libnss_wrapper.so
-  export NSS_WRAPPER_PASSWD=/var/lib/pgsql/passwd
+  export NSS_WRAPPER_PASSWD=${HOME}/passwd
   export NSS_WRAPPER_GROUP=/etc/group
 }
 
