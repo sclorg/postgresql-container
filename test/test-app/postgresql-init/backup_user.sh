49a01540bb31e65977d0daa6b06c02b8619e1f5f
Pavel Raiskup <praiskup@redhat.com>
2018-02-07 18:05:16 +0100

    tests: make sure that s2i test tests something
    Previously the test have not tested that the backup user was
    created and can login.
    
    Also implement cleanup hook for the s2i image, which now has
    randomly generated name.
    
    Implemented "create tempfile" method with garbage collector.

--- test/test-app/postgresql-init/backup_user.sh
+++ test/test-app/postgresql-init/backup_user.sh
@@ -2,7 +2,11 @@
 
 [[ -v POSTGRESQL_BACKUP_USER && -v POSTGRESQL_BACKUP_PASSWORD ]] || usage "You have to set all variables for user for doing backup: POSTGRESQL_BACKUP_USER, POSTGRESQL_BACKUP_PASSWORD"
 
-# create backup user
-
-psql -c "CREATE USER $POSTGRESQL_BACKUP_USER SUPERUSER  password '$POSTGRESQL_BACKUP_PASSWORD';"
-psql -c "ALTER USER $POSTGRESQL_BACKUP_USER set default_transaction_read_only = on;"
+# create backup user with 'backup' database
+psql --variable=user="$POSTGRESQL_BACKUP_USER" \
+     --variable=password="$POSTGRESQL_BACKUP_PASSWORD" \
+     <<<"
+CREATE USER :user SUPERUSER  password :'password';
+CREATE DATABASE backup OWNER = :user;
+ALTER USER :user set default_transaction_read_only = on;
+"
