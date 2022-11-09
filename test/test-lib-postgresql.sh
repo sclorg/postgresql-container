d03a6b41c08ac657a57e31897c62858e500b0f9f
Petr "Stone" Hracek <phracek@redhat.com>
2022-11-09 12:06:38 +0100

    Add tag for RHEL9
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test-lib-postgresql.sh
+++ test/test-lib-postgresql.sh
@@ -31,6 +31,8 @@ function test_postgresql_imagestream() {
   local tag="-el7"
   if [ "${OS}" == "rhel8" ]; then
     tag="-el8"
+  elif [ "${OS}" == "rhel9" ]; then
+    tag="-el9"
   fi
   ct_os_test_image_stream_template "${THISDIR}/imagestreams/postgresql-${OS%[0-9]*}.json" "${THISDIR}/examples/postgresql-ephemeral-template.json" postgresql "-p POSTGRESQL_VERSION=${VERSION}${tag}"
 }
