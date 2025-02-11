371cad3cd50582e5c722531d93b222e0133bb328
Petr "Stone" Hracek <phracek@redhat.com>
2025-02-11 09:22:10 +0100

    Fix postgresql-container test for Helm Charts.
    The helm charts have been renamed
    from `postgresql-*` -> `redhat-postgresql-*`
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_shared_helm_postgresql_imagestreams.py
+++ test/test_shared_helm_postgresql_imagestreams.py
@@ -18,7 +18,7 @@ test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
 class TestHelmRHELPostgresqlImageStreams:
 
     def setup_method(self):
-        package_name = "postgresql-imagestreams"
+        package_name = "redhat-postgresql-imagestreams"
         path = test_dir
         self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
         self.hc_api.clone_helm_chart_repo(
