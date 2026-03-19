88cdbf273abc1bb8af4732082d7363b6ad995f74
Petr "Stone" Hracek <phracek@redhat.com>
2026-03-19 13:45:10 +0100

    Rename constanst.py to conftest.py
    Update also OpenShift part
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_ocp_psql_imagestream_template.py
+++ test/test_ocp_psql_imagestream_template.py
@@ -1,45 +1,28 @@
-import os
-import sys
-
 import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
-from container_ci_suite.utils import check_variables
 
-from constants import TAGS
-
-if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
-    sys.exit(1)
-
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-OS = os.getenv("TARGET")
-
-TAG = TAGS.get(OS)
+from conftest import VARS
 
 
 class TestPostgreSQLImagestreamTemplate:
-
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="postgresql", version=VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(
+            pod_name_prefix="postgresql", version=VARS.VERSION, shared_cluster=True
+        )
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
     @pytest.mark.parametrize(
         "template",
-        [
-            "postgresql-ephemeral-template.json",
-            "postgresql-persistent-template.json"
-        ]
+        ["postgresql-ephemeral-template.json", "postgresql-persistent-template.json"],
     )
     def test_psql_imagestream_template(self, template):
-        os_name = ''.join(i for i in OS if not i.isdigit())
+        os_name = "".join(i for i in VARS.OS if not i.isdigit())
         assert self.oc_api.deploy_image_stream_template(
             imagestream_file=f"imagestreams/postgresql-{os_name}.json",
             template_file=f"examples/{template}",
-            app_name=self.oc_api.pod_name_prefix
+            app_name=self.oc_api.pod_name_prefix,
         )
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
--- test/test_ocp_psql_template.py
+++ test/test_ocp_psql_template.py
@@ -1,59 +1,44 @@
-import os
-import sys
-
 import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
-from container_ci_suite.utils import check_variables
 
-from constants import TAGS
-
-if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
-    sys.exit(1)
-
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-OS = os.getenv("TARGET")
-
-TAG = TAGS.get(OS)
+from conftest import VARS
 
 
 class TestPostgreSQLDeployTemplate:
-
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="postgresql-testing", version=VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(
+            pod_name_prefix="postgresql-testing",
+            version=VARS.VERSION,
+            shared_cluster=True,
+        )
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
     @pytest.mark.parametrize(
         "template",
-        [
-            "postgresql-ephemeral-template.json",
-            "postgresql-persistent-template.json"
-        ]
+        ["postgresql-ephemeral-template.json", "postgresql-persistent-template.json"],
     )
     def test_psql_template_inside_cluster(self, template):
-        short_version = VERSION.replace(".", "")
+        short_version = VARS.VERSION.replace(".", "")
         assert self.oc_api.deploy_template_with_image(
-            image_name=IMAGE_NAME,
+            image_name=VARS.IMAGE_NAME,
             template=f"examples/{template}",
             name_in_template="postgresql",
             openshift_args=[
-                f"POSTGRESQL_VERSION={VERSION}",
+                f"POSTGRESQL_VERSION={VARS.VERSION}",
                 f"DATABASE_SERVICE_NAME={self.oc_api.pod_name_prefix}",
-                f"POSTGRESQL_USER=testu",
-                f"POSTGRESQL_PASSWORD=testp",
-                f"POSTGRESQL_DATABASE=testdb"
-            ]
+                "POSTGRESQL_USER=testu",
+                "POSTGRESQL_PASSWORD=testp",
+                "POSTGRESQL_DATABASE=testdb",
+            ],
         )
 
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
         assert self.oc_api.check_command_internal(
-            image_name=f"registry.redhat.io/{OS}/postgresql-{short_version}",
+            image_name=f"registry.redhat.io/{VARS.OS}/postgresql-{short_version}",
             service_name=self.oc_api.pod_name_prefix,
             cmd="PGPASSWORD=testp pg_isready -t 15 -h <IP> -U testu -d testdb",
-            expected_output="accepting connections"
+            expected_output="accepting connections",
         )
